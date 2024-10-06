004440f78c840c5e4f5e7c753dc06a287fa92f27
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-05 22:48:40 -0700

    better filenames for uploading

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,9 @@ import { promisify } from "util";
 import prisma from "./db";
 
 const writeFile = promisify(fs.writeFile);
- 
+
+// https://stackoverflow.com/a/35922073
+const today = new Date().toISOString().slice(0, 10);
 // Code from Vercel
 // Licensed under MIT
 // https://github.com/vercel/next.js/blob/canary/examples/server-actions-upload/app/action.ts
@@ -14,9 +16,10 @@ export async function uploadImage(formData: FormData) {
     const image = formData.get("file") as File;
     const patientId = formData.get("patientId");
     const analysisId = formData.get("analysisId");
+    
 
     const fileBuffer = await image.arrayBuffer();
-    const imagePath = path.join(`/images/${image.name}`);
+    const imagePath = path.join(`/images/Analysis${analysisId}_Patient${patientId}_${today}${path.extname(image.name)}`);
     
     await writeFile(imagePath, Buffer.from(fileBuffer));
 
@@ -55,7 +58,7 @@ export async function submitReport(
     });
     return report;
 }
-export async function deleteReport( analysisId: number) {
+export async function deleteReport( analysisId: number ) {
     const deleteReport = prisma.report.delete({
         "where": {
             id: analysisId
