d388ee6ea4708fe078117068f6cec5f27d819de9
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-06 17:16:47 -0700

    Better uploading by separating the form

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,18 +1,9 @@
-import { uploadImage } from "../lib/actions";
+import Upload from "./upload";
 export default async function Page() {
     return (
         <>
             <h1>Upload OSCC image</h1>
-            <form action={uploadImage}>
-                <div>Patient ID:</div>
-                <input type="text" name="patientId" />
-                <div>Analysis ID:</div>
-                <input type="text" name="analysisId" />
-                <div>Upload file:</div>
-                <input type="file" name="file" />
-
-                <input type="submit" value="Upload!"/>
-            </form>
+            <Upload />
         </>
     );
 }
