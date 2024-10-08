83939d1c9da6168b19e3e6020d1ab3623b173324
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-08 12:05:30 -0700

    set up patient form

--- app/demo/add-patient.tsx
+++ app/demo/add-patient.tsx
@@ -1,11 +1,23 @@
 import { addPatient } from "../lib/actions";
 async function formAction(formData: FormData) {
     await addPatient(formData);
+    alert("Created a patient.");
 }
 export default function AddPatientForm() {
     return (
         <form action={formAction}>
-
+            <div>First Name</div>
+            <input type="text" name="firstName" />
+            <div>Middle name</div>
+            <input type="text" name="middleName" />
+            <div>Last name</div>
+            <input type="text" name="lastName" />
+            <div>Date of birth</div>
+            <input type="date" name="dateOfBirth" />
+            <div>Sex (0/1)</div>
+            <input type="number" name="lastName" />
+            <div>Assigned user ID</div>
+            <input type="text" name="assignedUser" />
         </form>
     );
 }
