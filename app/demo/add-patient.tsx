f293cae615358cc3d6ed3d9628f033b52a6f6f8a
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-08 11:20:20 -0700

    create add patient form

---
+++ app/demo/add-patient.tsx
@@ -0,0 +1,11 @@
+import { addPatient } from "../lib/actions";
+async function formAction(formData: FormData) {
+
+}
+export default function AddPatientForm() {
+    return (
+        <form action={formAction}>
+
+        </form>
+    );
+}
