59e5276cec8a9bd8cd57c0124eca54a542d37993
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-10 23:41:26 -0700

    change add patient fields to required

--- app/demo/add-patient.tsx
+++ app/demo/add-patient.tsx
@@ -7,17 +7,17 @@ export default function AddPatientForm() {
     return (
         <form action={formAction}>
             <div>First Name</div>
-            <input type="text" name="firstName" />
+            <input type="text" name="firstName" required />
             <div>Middle name</div>
-            <input type="text" name="middleName" />
+            <input type="text" name="middleName" required />
             <div>Last name</div>
-            <input type="text" name="lastName" />
+            <input type="text" name="lastName" required />
             <div>Date of birth</div>
-            <input type="date" name="dateOfBirth" />
+            <input type="date" name="dateOfBirth" required/>
             <div>Sex (0/1)</div>
-            <input type="number" name="lastName" />
+            <input type="number" name="sex" required />
             <div>Assigned user ID</div>
-            <input type="text" name="assignedUser" />
+            <input type="text" name="assignedUser" required />
         </form>
     );
 }
