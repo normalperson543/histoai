667d48f952157dae7a47b0403fa7bcca4c7bdfaa
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-11 17:34:58 -0700

    change out sex fields to radio fields and submit button

--- app/demo/add-patient.tsx
+++ app/demo/add-patient.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { addPatient } from "../lib/actions";
 async function formAction(formData: FormData) {
     await addPatient(formData);
@@ -14,10 +15,16 @@ export default function AddPatientForm() {
             <input type="text" name="lastName" required />
             <div>Date of birth</div>
             <input type="date" name="dateOfBirth" required/>
-            <div>Sex (0/1)</div>
-            <input type="number" name="sex" required />
+            <div>Sex</div>
+            <input type="radio" name="sex" value="male" id="male-sex" required />
+            <label htmlFor="male-sex">Male</label>
+            <input type="radio" name="sex" value="female" id="female-sex" required />
+            <label htmlFor="female-sex">Female</label>
+            <input type="radio" name="sex" value="unspecified" id="unspecified-sex" required />
+            <label htmlFor="unspecified-sex">Unspecified</label>
             <div>Assigned user ID</div>
             <input type="text" name="assignedUser" required />
+            <input type="submit" value="Submit" />
         </form>
     );
 }
