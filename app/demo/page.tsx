9a2b4abca4e6c5fb9b580496d6fcb40d7e1486a1
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-10-11 17:35:13 -0700

    add patient form to demo page

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,9 +1,11 @@
 import Upload from "./upload";
+import AddPatientForm from "./add-patient";
 export default async function Page() {
     return (
         <>
             <h1>Upload OSCC image</h1>
             <Upload />
+            <AddPatientForm />
         </>
     );
 }
