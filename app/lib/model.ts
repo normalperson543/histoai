5c2902661624d6c3e6c91304d1bbfc995f3bad42
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2024-11-27 17:39:10 -0800

    update model logic again?

--- app/lib/model.ts
+++ app/lib/model.ts
@@ -1,7 +1,7 @@
 'use client';
 import * as tf from "@tensorflow/tfjs";
 export default async function predictOSCC(imageElement: HTMLImageElement) {
-    const model = await tf.loadLayersModel('/model/model.json');
+    const model = await tf.loadGraphModel('/model/model.json');
     const imageTensor = tf.browser.fromPixels(imageElement);
     let resizedTensorFrame = tf.image.resizeBilinear(
         imageTensor, 
@@ -12,12 +12,15 @@ export default async function predictOSCC(imageElement: HTMLImageElement) {
     const prediction = await (model.predict(resizedTensorFrame.expandDims()) as tf.Tensor).squeeze();
     const predictionArray = prediction.arraySync() as number;
     let osccDetected;
+    let confidenceRate;
     if (predictionArray >= 0.5) {
         osccDetected = true;
+        confidenceRate = (predictionArray - 0.5) * 2
     }
     else {
         osccDetected = false;
+        confidenceRate = (0.5 - predictionArray) * 2
     }
     /*https://codelabs.developers.google.com/tensorflowjs-transfer-learning-teachable-machine#13 */
-    return {prediction: osccDetected, confidenceRate: predictionArray};
+    return {prediction: osccDetected, confidenceRate: confidenceRate};
 }
