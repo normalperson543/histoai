d6a84290de376d3ec8734b914ff6107da7c151e3
ElectricalRay <rayyankhalique@gmail.com>
2024-12-05 01:39:09 -0800

    resolved minor ui issues

--- app/dashboard/patients/page.tsx
+++ app/dashboard/patients/page.tsx
@@ -10,8 +10,8 @@ export default async function PatientDataPage() {
   const patients = await findAllPatients();
   return (
     <main>
-      <h2 className='text-3xl pt-10'>Registered Patients</h2>
-      <ul className='block w-[50%] mx-auto mt-10 divide-y divide-hblue/[0.25] border-y border-hblue-light/[0.5] shadow-lg'>
+      <h2 className='text-3xl pt-10 ml-5'>Registered Patients</h2>
+      <ul className='block w-[50%] ml-5 mt-10 divide-y divide-hblue/[0.25] border-y border-hblue-light/[0.5] shadow-lg'>
         <li className='p-1 bg-hblue-light/[0.2] px-2'>
           <div className='flex col-2'>
             <p className='text-left w-[50%]'>Patient Name</p>
@@ -42,9 +42,9 @@ export default async function PatientDataPage() {
           )
         })}
       </ul>
-      <div className='relative'>
+      <div className='relative w-[50%] ml-5'>
         <Link href="/dashboard/patients/new">
-          <button className='border rounded-md shadow-lg bg-hblue-light/[0.4] px-1 absolute top-5 right-[25%]'>Add Patient</button>
+          <button className='border rounded-md shadow-lg bg-hblue-light/[0.4] px-1 absolute top-5 right-0'>Add Patient</button>
         </Link>
       </div>
     </main>
--- app/ui/dashboard/reports/report-list.tsx
+++ app/ui/dashboard/reports/report-list.tsx
@@ -17,8 +17,8 @@ export default function ReportContainer({reports, reportItems}: {reports: {
 reportItems: any[]}) {
     return (
         <main>
-            <h1 className='text-center text-5xl pt-10'>Reports</h1>
-            <ul className='block w-[50%] mx-auto mt-10 divide-y divide-hblue/[0.25] border-y border-hblue-light/[0.5] shadow-lg'>
+            <h1 className='text-5xl pt-10 ml-5'>Reports</h1>
+            <ul className='block w-[50%] ml-5 mt-10 divide-y divide-hblue/[0.25] border-y border-hblue-light/[0.5] shadow-lg'>
                 <li className='p-1 bg-hblue-light/[0.2] px-2'>
                 <div className='flex col-2'>
                     <p className='text-left w-[50%]'>Patient Name Under Report</p>
@@ -28,9 +28,11 @@ reportItems: any[]}) {
                 {reportItems}
                 
             </ul>
-            <Link href="/dashboard/reports/new">
-                <button className='border rounded-md shadow-lg bg-hblue-light/[0.4] px-1'>Add Report</button>
-            </Link>
+            <div className="relative w-[50%] ml-5">
+                <Link href="/dashboard/reports/new">
+                    <button className='border rounded-md shadow-lg bg-hblue-light/[0.4] px-1 absolute top-5 right-0'>Add Report</button>
+                </Link>
+            </div>
         </main>
     )
 }
